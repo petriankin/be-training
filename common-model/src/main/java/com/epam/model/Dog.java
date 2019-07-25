35b47b3b4eed00aa818f1826d95fa851c0cf4ecc
Petriankin <sergei_petriankin@epam.com>
2019-07-25 09:17:42 +0300

    model validation update

--- common-model/src/main/java/com/epam/model/Dog.java
+++ common-model/src/main/java/com/epam/model/Dog.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
@@ -19,15 +20,18 @@ public class Dog {
 
     private UUID id;
 
+    @NotNull
     @Size(min = 1, max = 100, message = "Name must be between 1 and 100 symbols long")
     private String name;
 
     @Past(message = "Date of Birth must be in the past")
     private LocalDate dateOfBirth;
 
+    @NotNull
     @Positive(message = "Height must be greater than zero")
     private int height;
 
+    @NotNull
     @Positive(message = "Weight must be greater than zero")
     private int weight;
 }
