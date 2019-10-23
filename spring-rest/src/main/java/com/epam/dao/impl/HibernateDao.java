49c3ecd15163e754782952f4e443c04689441dd7
Sergei Petriankin <sergei_petriankin@epam.com>
2019-10-23 10:46:49 +0300

    hibernate context fixes

--- spring-rest/src/main/java/com/epam/dao/impl/HibernateDao.java
+++ spring-rest/src/main/java/com/epam/dao/impl/HibernateDao.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -14,29 +15,32 @@ public class HibernateDao implements DogDao {
     private SessionFactory sessionFactory;
 
     @Override
+    @Transactional
     public Dog createDog(Dog dog) {
         getCurrentSession().save(dog);
         return dog;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Dog getDog(UUID id) {
         return getCurrentSession().get(Dog.class, id);
     }
 
     @Override
+    @Transactional
     public Dog updateDog(UUID id, Dog dog) {
         getCurrentSession().saveOrUpdate(dog);
         return dog;
     }
 
     @Override
+    @Transactional
     public void deleteDog(UUID id) {
         Dog dog = getCurrentSession().get(Dog.class, id);
         getCurrentSession().delete(dog);
     }
 
-    // FIXME: 10/23/2019
     private Session getCurrentSession() {
         return sessionFactory.getCurrentSession();
     }
